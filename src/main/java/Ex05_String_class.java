e1e322de55be893d6a474ea09665c7d78c22cd1d
BillionDollarSohee <blackteatime4@naver.com>
2025-08-30 08:44:45 +0900

    초기 프로젝트 생성

---
+++ src/main/java/Ex05_String_class.java
@@ -0,0 +1,16 @@
+/*
+8(기본타입) + 1(String)
+문자열을 다룰 때 그냥 쓰기보다는 조작 (자르고, 합치고, 나누고, 기능)을 하기 쉽게
+String 은 수많은 일반함수과 static 함수를 가지고 있다. -> String은 클래스로 설계했다.
+ */
+
+public class Ex05_String_class {
+
+//    String str = "";
+//    String[] str1 = {"ad","dd","fd"};
+//
+//    for (String s : str1) {
+//        System.out.println(s);
+//    }
+
+}
