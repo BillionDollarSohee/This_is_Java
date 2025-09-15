50ce1b5e3da21ae76c18d1014ef641c21e9fc525
BillionDollarSohee <blackteatime4@naver.com>
2025-09-15 16:56:47 +0900

    No10

---
+++ src/main/java/org/everydayAlgorithm/No10.java
@@ -0,0 +1,31 @@
+package org.everydayAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[input][input];
+
+        int start = 1;
+        for (int i = 0; i < input; i ++) {
+            for (int j = 0; j < input; j ++) {
+                arr[i][j] = start;
+                start++;
+            }
+        }
+
+
+        for (int i = 0; i < input; i++) {
+            for (int j = 0; j < input; j++) {
+                System.out.printf("%4d", arr[i][j]); 
+            }
+            System.out.println();
+        }
+
+    }
+}
