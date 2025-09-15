df0f909fa125b05da5e0dfadf4b9e03eed5b6702
BillionDollarSohee <blackteatime4@naver.com>
2025-09-15 17:12:44 +0900

    No11

--- src/main/java/org/everydayAlgorithm/No11.java
+++ src/main/java/org/everydayAlgorithm/No11.java
@@ -4,22 +4,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class No10 {
+public class No11 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int input = Integer.parseInt(br.readLine());
 
         int[][] arr = new int[input][input];
-
         int start = 1;
         for (int i = 0; i < input; i ++) {
-            for (int j = 0; j < input; j ++) {
-                arr[i][j] = start;
-                start++;
+            if( (i+1) %  2 == 0) {
+                for (int j = input - 1; j >= 0; j--) {
+                    arr[i][j] = start;
+                    start++;
+                }
+            } else {
+                for (int j = 0; j < input; j ++) {
+                    arr[i][j] = start;
+                    start++;
+                }
             }
         }
 
-
+        // 출력
         for (int i = 0; i < input; i++) {
             for (int j = 0; j < input; j++) {
                 System.out.printf("%4d", arr[i][j]);
