57b4cbdcc3a542d9427ae445b4e9adf9d0345c6a
BillionDollarSohee <blackteatime4@naver.com>
2025-09-23 20:05:28 +0900

    09/23 last

--- src/main/java/org/everydayAlgorithm/twenty/No18.java
+++ src/main/java/org/everydayAlgorithm/twenty/No18.java
@@ -4,19 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class No17 {
+public class No18 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
 
         String star = "*";
         String blank = " ";
-
-        for (int i = 0; i < n; i++) {
-            for (int k = 0; k < i; k++) {
+        for (int i = 0; i < n; i ++) {
+            for (int k = 0; k < n - i; k++) {
                 System.out.print(blank);
             }
-            for (int k = 0; k < n - i; k++) {
+            for (int k = 0; k < i + 1; k++) {
                 System.out.print(star);
             }
             System.out.println();
