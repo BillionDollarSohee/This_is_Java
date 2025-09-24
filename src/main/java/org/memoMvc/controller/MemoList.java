fc77f621a3dbea38b32bdfd1cdaf2c4a71509ece
BillionDollarSohee <blackteatime4@naver.com>
2025-09-24 10:35:58 +0900

    경로 변경

--- src/main/java/org/memoMvc/controller/MemoList.java
+++ src/main/java/org/memoMvc/controller/MemoList.java
@@ -1,14 +1,17 @@
 package org.memoMvc.controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.memoMvc.dao.MemoDao;
+import org.memoMvc.dto.Memo;
 
 import java.io.IOException;
 import java.io.Serial;
 import java.sql.SQLException;
+import java.util.List;
 
 public class MemoList extends HttpServlet {
     @Serial
@@ -19,16 +22,34 @@ public class MemoList extends HttpServlet {
     }
 
     private void doProcess(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException {
+            throws ServletException, IOException {
         System.out.println("목록보기");
 
-        // 요청에 따른판단이 필요없다.
-
-        // 목록보기
         MemoDao memoDao = new MemoDao();
+
+        try {
+            List<Memo> memoList = memoDao.getMemoList();
+            request.setAttribute("memoList", memoList); // 데이터 저장
+
+            // 뷰 지정
+            RequestDispatcher dis = request.getRequestDispatcher("/memoMvc/memoList.jsp");
+            dis.forward(request, response);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            // 에러 페이지로 포워드
+            RequestDispatcher dis = request.getRequestDispatcher("/views/error.jsp");
+            dis.forward(request, response);
+        }
     }
 
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
     }
