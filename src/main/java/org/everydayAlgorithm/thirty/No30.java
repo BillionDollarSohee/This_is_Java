7fb5e172125e8d20a2e32f09f767ab22fbb70ab6
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2025-10-30 23:19:44 +0900

    클래스 위치 수정

--- src/main/java/org/everydayAlgorithm/thirty/No30.java
+++ src/main/java/org/everydayAlgorithm/thirty/No30.java
@@ -8,6 +8,20 @@ public class No30 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            result.append(i);
+
+            if (i == n) break;
+
+            if (i % 2 == 1) result.append("+");
+            else result.append("-");
+        }
+
+        System.out.println(result);
 
     }
 }
